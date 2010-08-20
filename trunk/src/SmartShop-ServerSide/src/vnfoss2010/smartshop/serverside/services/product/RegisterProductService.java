da43940e17a3c0a9022e971febfd4b85f65595a5
aizikko@gmail.com <aizikko@gmail.com@17933ba0-26e0-3388-bfae-da3ed41a6e92>
2010-08-20 10:00:05 +0000

    Fix for register product use gson

--- trunk/src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/services/product/RegisterProductService.java
+++ trunk/src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/services/product/RegisterProductService.java
@@ -1,5 +1,6 @@
 package vnfoss2010.smartshop.serverside.services.product;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.logging.Level;
@@ -46,75 +47,21 @@ public class RegisterProductService extends BaseRestfulService {
 			json = new JSONObject(content);
 		} catch (Exception e) {
 		}
-
 		Gson gson = new Gson();
-		Product product2 = gson.fromJson(content, Product.class);
-
-		log.info(product2.toString());
-		log.log(Level.SEVERE, product2.toString());
-
-		String userName = getParameterWithThrow("username", params, json);
-
-		Product product = new Product();
-		product.setAddress(getParameterWithThrow("address", params, json));
-
-		product.setIs_vat(Boolean.getBoolean((getParameterWithThrow("is_vat",
-				params, json))));
-		product.setLat((Double.parseDouble(getParameterWithThrow("lat", params,
-				json))));
-		product.setLng(Double.parseDouble((getParameterWithThrow("lng", params,
-				json))));
-		product.setName((getParameterWithThrow("name", params, json)));
-		product.setOrigin((getParameterWithThrow("origin", params, json)));
-		product.setPrice(Double.parseDouble((getParameterWithThrow("price",
-				params, json))));
-		product.setQuantity(Integer.parseInt((getParameterWithThrow("quantity",
-				params, json))));
-		product.setUsername(userName);
-
-		JSONArray jsonAttArray = getJSONArrayWithThrow("atts", json);
-		int attsLength = jsonAttArray.length();
-		HashSet<Attribute> attsList = new HashSet<Attribute>();
-		for (int i = 0; i < attsLength; i++) {
-			JSONObject anAttJSON = jsonAttArray.getJSONObject(i);
-			String catKey = getParameterWithThrow("key_cat", params, anAttJSON);
-			Attribute attribute = null;
-			ServiceResult<Boolean> resultInsertAtt = null;
-
-			attribute = new Attribute(catKey, getParameterWithThrow("name",
-					params, anAttJSON), getParameterWithThrow("value", params,
-					anAttJSON), userName);
-			resultInsertAtt = dbatt.insertAttribute(attribute);
-			if (resultInsertAtt.isOK()) {
-				attsList.add(attribute);
-			}
-
-			if (resultInsertAtt == null || resultInsertAtt.isOK() == false) {
-				throw missingParameter("catKey in attributes");
-			}
+		Product product = gson.fromJson(content, Product.class);
+		ServiceResult<ArrayList<Category>> listCategories = dbcat
+				.findCategories(product.getSetCategoryKeys());
+		if (listCategories.isOK() == false) {
+			jsonReturn.put("errCode", 1);
+			jsonReturn.put("message", listCategories.getMessage());
 		}
-		// PMF.get().getPersistenceManager().flush();
-		product.setSetAttributes(attsList);
 
-		JSONArray jsonCatArray = getJSONArrayWithThrow("cats", json);
-		HashSet<String> catSet = new HashSet<String>();
-		for (int i = 0; i < jsonCatArray.length(); i++) {
-			JSONObject jsonCat = jsonCatArray.getJSONObject(i);
-			String catID = (String) jsonCat.get("id");
-			ServiceResult<Category> resultFindCat = dbcat.findCategory(catID);
-			if (resultFindCat == null || resultFindCat.isOK() == false) {
-				throw missingParameter("cats id");
-			}
-			catSet.add(catID);
+		if (jsonReturn.has("errCode") == false) {
+			ServiceResult<Long> result = db.insertProduct(product);
+
+			jsonReturn.put("errCode", result.isOK() ? 0 : 1);
+			jsonReturn.put("message", result.getMessage());
 		}
-		product.setSetCategoryKeys(catSet);
-		product.setGeocells(GeocellManager.generateGeoCell(product
-				.getLocation()));
-
-		ServiceResult<Long> result = db.insertProduct(product);
-
-		jsonReturn.put("errCode", result.isOK() ? 0 : 1);
-		jsonReturn.put("message", result.getMessage());
 		return jsonReturn.toString();
 		// return "here " + result.getMessage() + "_" + result.getResult();
 	}
