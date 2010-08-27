882eb4c0bb014251c0c291b8e66d79202b4a4356
vo.mita.ov <vo.mita.ov@gmail.com>
2010-08-27 04:49:54 +0000

    Add Email Services: - Send Email to Admin - Send Email to anyone

---
+++ src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/services/mail/SendEmailService.java
@@ -0,0 +1,44 @@
+package vnfoss2010.smartshop.serverside.services.mail;
+
+import java.util.Map;
+
+import vnfoss2010.smartshop.serverside.database.ServiceResult;
+import vnfoss2010.smartshop.serverside.mail.MailUtils;
+import vnfoss2010.smartshop.serverside.services.BaseRestfulService;
+import vnfoss2010.smartshop.serverside.services.exception.RestfulException;
+
+import com.google.appengine.repackaged.org.json.JSONObject;
+
+public class SendEmailService extends BaseRestfulService{
+
+	public SendEmailService(String serviceName) {
+		super(serviceName);
+	}
+
+	@Override
+	public String process(Map<String, String[]> params, String content)
+			throws Exception, RestfulException {
+		JSONObject json = null;
+		try {
+			json = new JSONObject(content);
+		} catch (Exception e) {
+		}
+		
+		String sender = getParameter("sender", params, json);
+		String title = getParameter("title", params, json);
+		String to = getParameterWithThrow("to", params, json);
+		String cont = getParameter("content", params, json);
+		
+		ServiceResult<Void> result = MailUtils.sendEmail(sender, to, title, cont);
+		
+		JSONObject jsonReturn = new JSONObject();
+		if (result.isOK()) {
+			jsonReturn.put("errCode", 0);
+		} else {
+			jsonReturn.put("errCode", 1);
+		}
+		jsonReturn.put("message", result.getMessage());
+		return jsonReturn.toString();
+	}
+
+}
