afa00df2442082930eeb0675fe643275d10f06bc
aizikko@gmail.com <aizikko@gmail.com@17933ba0-26e0-3388-bfae-da3ed41a6e92>
2010-08-31 15:18:42 +0000

    fix sample product, generate geocell for it

--- trunk/src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/services/test/InsertSampleProductService.java
+++ trunk/src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/services/test/InsertSampleProductService.java
@@ -2,14 +2,19 @@ package vnfoss2010.smartshop.serverside.services.test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
+import com.beoui.geocell.GeocellManager;
+
+import vnfoss2010.smartshop.serverside.Global;
 import vnfoss2010.smartshop.serverside.database.ProductServiceImpl;
 import vnfoss2010.smartshop.serverside.database.entity.Product;
 import vnfoss2010.smartshop.serverside.services.BaseRestfulService;
 import vnfoss2010.smartshop.serverside.services.exception.RestfulException;
 import vnfoss2010.smartshop.serverside.test.SampleData;
 
-public class InsertSampleProductService extends BaseRestfulService{
+public class InsertSampleProductService extends BaseRestfulService {
+	Logger log = Logger.getLogger(InsertSampleProductService.class.getName());
 
 	public InsertSampleProductService(String serviceName) {
 		super(serviceName);
@@ -19,12 +24,14 @@ public class InsertSampleProductService extends BaseRestfulService{
 	public String process(Map<String, String[]> params, String content)
 			throws Exception, RestfulException {
 		ProductServiceImpl db = ProductServiceImpl.getInstance();
-		
+
 		List<Product> list = SampleData.getSampleProducts();
-		for (Product product : list){
+		for (Product product : list) {
+			product.setGeocells(GeocellManager.generateGeoCell(product
+					.getLocation()));
 			db.insertProduct(product);
 		}
-		
+
 		return "Done";
 	}
 
