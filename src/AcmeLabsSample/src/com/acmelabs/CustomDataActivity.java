030adb3f29f32f858ade49dd443611d20bbc4763
condorhero01 <condorhero01@17933ba0-26e0-3388-bfae-da3ed41a6e92>
2010-10-02 11:56:32 +0000

    Xtify example

---
+++ src/AcmeLabsSample/src/com/acmelabs/CustomDataActivity.java
@@ -0,0 +1,27 @@
+package com.acmelabs;
+
+import com.xtify.android.sdk.NotificationActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+/**
+ * This is a sample Activity that responds to a custom notification action. 
+ * The data is retrieved from the Intent and in this case, it is displayed in a text box.
+ * To launch this Activity, your notification would set the action to "com.acmelabs.DISPLAY_CUSTOM_DATA" in order 
+ * to match the intent filter defined in AndroidManifest.xml.
+ */
+public class CustomDataActivity extends NotificationActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.custom_data);
+		Intent intent = getIntent();
+		String data = decodeUriData(intent.getData());
+		if (data != null) {
+			EditText dataTextBox = (EditText)findViewById(R.id.dataTextBox);
+			dataTextBox.setText(data);
+		}
+	}
+};
