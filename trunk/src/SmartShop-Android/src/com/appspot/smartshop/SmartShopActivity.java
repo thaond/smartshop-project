7747f5ce42f1025d86e98b54c11b589573f7fb4d
vo.mita.ov@gmail.com <vo.mita.ov@gmail.com@17933ba0-26e0-3388-bfae-da3ed41a6e92>
2010-10-20 17:28:24 +0000

    Integrate with Xtify

--- trunk/src/SmartShop-Android/src/com/appspot/smartshop/SmartShopActivity.java
+++ trunk/src/SmartShop-Android/src/com/appspot/smartshop/SmartShopActivity.java
@@ -1,92 +1,134 @@
 package com.appspot.smartshop;
 
-import java.util.Set;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.appspot.smartshop.adapter.MainAdapter;
-import com.appspot.smartshop.ui.user.subcribe.SubcribeActivity;
-import com.appspot.smartshop.ui.user.subcribe.UserSubcribeListActivity;
-import com.appspot.smartshop.utils.CategoriesDialog;
 import com.appspot.smartshop.utils.DataLoader;
 import com.appspot.smartshop.utils.Global;
 import com.appspot.smartshop.utils.JSONParser;
-import com.appspot.smartshop.utils.NotifyingService;
 import com.appspot.smartshop.utils.RestClient;
 import com.appspot.smartshop.utils.SimpleAsyncTask;
 import com.appspot.smartshop.utils.URLConstant;
-import com.appspot.smartshop.utils.CategoriesDialog.CategoriesDialogListener;
+import com.xtify.android.sdk.PersistentLocationManager;
 
 public class SmartShopActivity extends ListActivity {
 	public static final String TAG = "[SmartShopActivity]";
-	
+
 	public static final String PRODUCT = "product";
 	public static final String PAGE = "page";
-	
+
 	private SimpleAsyncTask task;
+	private PersistentLocationManager persistentLocationManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Global.application = this;
-		
+
 		ListView listView = getListView();
 		listView.setAdapter(new MainAdapter(this));
-		
+
 		/**************************** Init data *********************************/
 		Log.d(TAG, "[START NOTIFICATION SERVICE]");
 		// TODO start service notification
-//		startService(new Intent(this, NotifyingService.class));
-		
+		// startService(new Intent(this, NotifyingService.class));
+
 		if (Global.mapParentCategories.size() != 0) {
 			Log.d(TAG, "[DONT NEED TO GET CATEGORIES LIST]");
 		} else {
 			Log.d(TAG, "[GET CATEGORIES LIST]");
-			task = new SimpleAsyncTask(getString(R.string.init_data), this, new DataLoader() {
-				
-				@Override
-				public void updateUI() {
-					// TODO test related with categories dialog
-//					Intent intent = new Intent(SmartShopActivity.this, UserSubcribeListActivity.class);
-//					startActivity(intent);
-				}
-				
-				@Override
-				public void loadData() {
-					getCategoriesList();
-				}
-			});
+			task = new SimpleAsyncTask(getString(R.string.init_data), this,
+					new DataLoader() {
+
+						@Override
+						public void updateUI() {
+							// TODO test related with categories dialog
+							// Intent intent = new
+							// Intent(SmartShopActivity.this,
+							// UserSubcribeListActivity.class);
+							// startActivity(intent);
+						}
+
+						@Override
+						public void loadData() {
+							getCategoriesList();
+						}
+					});
 			task.execute();
 		}
+
+		Thread xtifyThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Log.e(TAG, "Thread run");
+				persistentLocationManager = new PersistentLocationManager(
+						SmartShopActivity.this);
+				persistentLocationManager
+						.setNotificationIcon(R.drawable.notification);
+				persistentLocationManager
+						.setNotificationDetailsIcon(R.drawable.icon);
+				boolean trackLocation = persistentLocationManager
+						.isTrackingLocation();
+				boolean deliverNotifications = persistentLocationManager
+						.isDeliveringNotifications();
+				Log.e(TAG, persistentLocationManager.getUserKey());
+				if (trackLocation || deliverNotifications) {
+					persistentLocationManager.startService();
+				}
+			}
+		});
+		xtifyThread.start(); // to avoid Android's application-not-responding
+								// dialog box, do non-essential work in another
+								// thread
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, 0, 0, getString(R.string.settings) );
+		menu.findItem(0).setIcon(R.drawable.menu_preferences);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case 0: {
+				persistentLocationManager.showSettingsActivity(this, true);
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private void getCategoriesList() {
 		// get parent categories
 		RestClient.getData(URLConstant.GET_PARENT_CATEGORIES, new JSONParser() {
-			
+
 			@Override
 			public void onSuccess(JSONObject json) throws JSONException {
 				JSONArray arr = json.getJSONArray("categories");
-				
+
 				int len = arr.length();
 				String name, key_cat;
 				for (int i = 0; i < len; ++i) {
 					name = arr.getJSONObject(i).getString("name");
 					key_cat = arr.getJSONObject(i).getString("key_cat");
-					
+
 					Global.mapParentCategories.put(key_cat, name);
 				}
 			}
-			
+
 			@Override
 			public void onFailure(String message) {
 				task.hasData = false;
@@ -94,33 +136,34 @@ public class SmartShopActivity extends ListActivity {
 				task.cancel(true);
 			}
 		});
-		
+
 		// get child categories
 		for (final String parentId : Global.mapParentCategories.keySet()) {
-			String url = String.format(URLConstant.GET_CHILD_CATEGORIES, parentId);
-			
+			String url = String.format(URLConstant.GET_CHILD_CATEGORIES,
+					parentId);
+
 			RestClient.getData(url, new JSONParser() {
-				
+
 				@Override
 				public void onSuccess(JSONObject json) throws JSONException {
 					JSONArray arr = json.getJSONArray("categories");
-					
+
 					int len = arr.length();
 					String name, key_cat;
 					String[] temp = new String[len];
 					for (int k = 0; k < len; ++k) {
 						name = arr.getJSONObject(k).getString("name");
 						key_cat = arr.getJSONObject(k).getString("key_cat");
-						
+
 						temp[k] = name;
-						
+
 						Global.mapChildrenCategories.put(key_cat, name);
 						Global.mapChildrenCategoriesName.put(name, key_cat);
 					}
-					
+
 					Global.listCategories.add(temp);
 				}
-				
+
 				@Override
 				public void onFailure(String message) {
 					task.hasData = false;
@@ -134,10 +177,10 @@ public class SmartShopActivity extends ListActivity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		
+
 		Log.d(TAG, "[STOP NOTIFICATION SERVICE]");
 		Global.isStop = true;
 		// TODO stop service notification
-//		stopService(new Intent(this, NotifyingService.class));
+		// stopService(new Intent(this, NotifyingService.class));
 	}
 }
