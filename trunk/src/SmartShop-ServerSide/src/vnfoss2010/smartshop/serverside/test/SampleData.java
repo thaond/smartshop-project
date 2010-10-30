fb815f8bdc061bc2b8cf53e74228bacf9e721b95
aizikko@gmail.com <aizikko@gmail.com@17933ba0-26e0-3388-bfae-da3ed41a6e92>
2010-10-30 13:12:03 +0000

    sample friend data

--- trunk/src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/test/SampleData.java
+++ trunk/src/SmartShop-ServerSide/src/vnfoss2010/smartshop/serverside/test/SampleData.java
@@ -14,25 +14,49 @@ public class SampleData {
 	public static ArrayList<UserInfo> getSampleListUserInfos() {
 		Calendar calendar = Calendar.getInstance();
 		ArrayList<UserInfo> listUserInfos = new ArrayList<UserInfo>();
-		listUserInfos.add(new UserInfo("hieu", "hieu", "Hieu", "Hua Phan Minh",
+		UserInfo user = null;
+		user = new UserInfo("hieu", "hieu", "Hieu", "Hua Phan Minh",
 				"123123123", "hieu@gmail.com", new Date(87, 1, 1),
-				"Tan Phu district", 10.213D, 106.123123D, ""));
+				"Tan Phu district", 10.213D, 106.123123D, "");
+		user.getSetFriendsUsername().add("tam");
+		user.getSetFriendsUsername().add("duc");
+		user.getSetFriendsUsername().add("loi");
+		user.getSetFriendsUsername().add("nghia");
+		listUserInfos.add(user);
 
-		listUserInfos.add(new UserInfo("tam", "tam", "Tam", "Vo Minh",
+		user = new UserInfo("tam", "tam", "Tam", "Vo Minh",
 				"123123123", "tam@gmail.com", new Date(89, 12, 22),
-				"Binh Tan district", 10.213D, 106.123123D, ""));
+				"Binh Tan district", 10.213D, 106.123123D, "");
+		user.getSetFriendsUsername().add("duc");
+		user.getSetFriendsUsername().add("hieu");
+		listUserInfos.add(user);
 
-		listUserInfos.add(new UserInfo("nghia", "nghia", "Nghia", "Le Trong",
+		
+		user = new UserInfo("nghia", "nghia", "Nghia", "Le Trong",
 				"123123123", "nghia@gmail.com", new Date(89, 1, 1),
-				"District 6", 10.213D, 106.123123D, ""));
+				"District 6", 10.213D, 106.123123D, "");
+		user.getSetFriendsUsername().add("tam");
+		user.getSetFriendsUsername().add("loi");
+		listUserInfos.add(user);
 
-		listUserInfos.add(new UserInfo("duc", "duc", "Duc", "Cao Tien",
+		
+		user = new UserInfo("duc", "duc", "Duc", "Cao Tien",
 				"123123123", "duc@gmail.com", new Date(89, 12, 8),
-				"District 10", 10.213D, 106.123123D, ""));
+				"District 10", 10.213D, 106.123123D, "");
+		user.getSetFriendsUsername().add("loi");
+		user.getSetFriendsUsername().add("duc");
+		user.getSetFriendsUsername().add("hieu");
+		user.getSetFriendsUsername().add("nghia");
+		listUserInfos.add(user);
 
-		listUserInfos.add(new UserInfo("loi", "loi", "Lợi", "Nguyễn Văn",
+		
+		user = new UserInfo("loi", "loi", "Lợi", "Nguyễn Văn",
 				"123123123", "loi@gmail.com", new Date(90, 1, 1),
-				"District 10", 10.213D, 106.123123D, ""));
+				"District 10", 10.213D, 106.123123D, "");
+		user.getSetFriendsUsername().add("duc");
+		user.getSetFriendsUsername().add("nghia");
+		user.getSetFriendsUsername().add("hieu");
+		listUserInfos.add(user);
 
 		return listUserInfos;
 	}
